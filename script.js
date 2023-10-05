f34ce24f38cfb7c17342ea0e0568d5452e563f47
sanakez <144197284+sanakez@users.noreply.github.com>
2023-10-05 20:32:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript to add smooth scrolling behavior
+document.querySelectorAll('.content-navigation a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
